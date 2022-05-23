9af1c194982845fa77242a3a02505c399dc4aaeb
Rosario Garufi <saro.garufi1@gmail.com>
2022-05-23 17:02:44 +0200

    Stampa informazioni all'interno struttura dati creata

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 $web_google =  [
     [
-        "domanda" => " <h1>Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?</h1>",
+        "domanda" => " <h2>Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?</h2>",
         "risposta" => " <p>La recente <span> decisione della Corte di giustizia dell'Unione europea </span> ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.</p>
             <p> Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.</p>
             <p>Per presentare una richiesta di rimozione, compila questo <span>modulo web</span>. Riceverai una risposta automatica che conferma la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di tempo perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche, ad esempio se riguardano frodi finanziarie, negligenza professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio (eletto o non eletto). Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d'accordo con la nostra valutazione, puoi rivolgerti all'Autorità garante per la protezione dei dati personali nel tuo paese.</p>
@@ -11,7 +11,7 @@ $web_google =  [
 
     ],
     [
-        "domanda" => "<h1>Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?</h1>",
+        "domanda" => "<h2>Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?</h2>",
         "risposta" => " <p> Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.</p>
             <p> Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.</p>
             
@@ -21,7 +21,7 @@ $web_google =  [
 
     ],
     [
-        "domanda" => "<h1>Perché il mio account è associato a un paese?</h1>",
+        "domanda" => "<h2>Perché il mio account è associato a un paese?</h2>",
         "risposta" => "<ol> Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:
            <li><ol>La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:</li>
            <li> Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.</li><br>
@@ -98,6 +98,48 @@ $web_google =  [
 </header>
 
 <hr>
+
+<main>
+
+<div class="container">
+    <?php
+    foreach ($web_google as $page) { ?>
+
+        <?php echo $page['domanda'] ?>
+        <?php echo $page['risposta'] ?>
+
+    <?php }; ?>
+
+    
+</div>
+</main>
+
+<footer>
+    <div class="container">
+        <div class="sinistra">
+            <ul class="list">
+                <li>
+                    <a href="#">Google</a>
+                </li>
+                <li>
+                    <a href="#">Tutto su Google</a>
+                </li>
+                <li>
+                    <a href="#">Privacy</a>
+                </li>
+                <li>
+                    <a href="#">Termini</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="destra">
+            <select name="Lingua" id="input-select">
+                <option value="Italiano"></option>
+            </select>
+        </div>
+    </div>
+</footer>
     
 </body>
 </html>
